f48865574886b4158238dfb190c4164a81f4b8d5
Weakldi <kristian@ArchNB.fritz.box>
2018-01-14 22:01:47 +0100

    < Bruch-Aufgabe

--- Bruch/src/Bruch.java
+++ Bruch/src/Bruch.java
@@ -3,26 +3,32 @@
  */
 public class Bruch {
 
-	/*
-	 * Hier Attributvereinbarungen ergaenzen.
+	/**
+	 * 
 	 */
-	
 	private int zaehler, nenner;
 	
-	/*
-	 * Hier Konstruktoren ergaenzen.
+
+	/**
+	 * Erzeugt einen neuen Bruch mit dem Zaehler 1 und dem Nenner 1.
+	 * Der Bruch representiert somit den Wert 1.
 	 */
-	
 	public Bruch() {
 		this.zaehler = this.nenner = 1;
 	}
 	
+	/**
+	 * Erzeugt einen neuen Bruch mit dem Zaehler zaehler und dem Nenner 1.
+	 * Der Bruch representiert den Wert des Zaehlers.
+	 * @param zaehler
+	 */
 	public Bruch(int zaehler) {
 		this.zaehler = zaehler;
 		this.nenner = 1;
 	}
 	
 	/**
+	 * Erzeugt einen neuen Bruch mit dem Zaehler zaehler und dem Nenner nenner.
 	 * @param zaehler
 	 * @param nenner
 	 */
@@ -32,21 +38,31 @@ public class Bruch {
 		this.zaehler = zaehler;
 		setNenner(nenner); // Kuertzt den Bruch direkt.
 	}
-	/*
-	 * Hier Methoden ergaenzen.
+
+	/**
+	 * Addiert den Bruch b2 zum Bruch.
+	 * @param b2 Anderer Summand
+	 * @return Referenz auf einen Bruch welcher die Summe enthaelt.
 	 */
-	
 	public Bruch add(Bruch b2){
 		int n3 = this.nenner*b2.getNenner();
 		int z3 = this.zaehler*b2.getNenner() + b2.getZaehler() * this.nenner;
 		return new Bruch(z3, n3);
 	}
-	
+	/**
+	 * Substituiert b2 vom Bruch.
+	 * @param b2 der Substituent
+	 * @return Differrenz Bruch-b2.
+	 */
 	public Bruch sub(Bruch b2) {
-		b2.setNenner(b2.getNenner()*-1);
-		return add(b2);
+		Bruch b3 = new Bruch(b2.zaehler, b2.nenner * -1);
+		return add(b3);
 	}
-	
+	/**
+	 * Berechent das Produkt des Bruches mit einem Anderen Bruch b2.
+	 * @param b2 anderer Bruch.
+	 * @return Referenz auf ein neuen Bruch welcher das Produkt enthaelt.
+	 */
 	public Bruch mul(Bruch b2) {
 		return new Bruch(
 				this.zaehler * b2.zaehler,
@@ -54,16 +70,40 @@ public class Bruch {
 				);
 	}
 	
+	/**
+	 * Berechent den Quotienten des Bruches mit einem Anderen Bruch b2.
+	 * @param b2 Bruch durch den geteilt werden soll.
+	 * @return Referenz auf ein neuen Bruch welcher den Quotienten enthaelt.
+	 */
 	public Bruch div(Bruch b2) {
 		return mul(b2.getKehrWert());
 	}
 	
+	/**
+	 * Berechent den Kehrwert des Bruches.
+	 * @return Referenz auf ein neuen Bruch welcher den Kehrwert des Bruches enthaelt.
+	 */
 	public Bruch getKehrWert() {
 		return new Bruch(nenner, zaehler);
 	}
 	
+	/**
+	 * Berechnet den groeßten gemeinsamen Teiler von a, b.
+	 * @param a Zahl 1
+	 * @param b Zahl 2
+	 * @return ggt
+	 */
 	int ggt(int a, int b) {
 		return b == 0 ? a : ggt(b, a % b);
+	}
+	
+	/**
+	 * Kuertzt den Bruch
+	 */
+	void kuerze() {
+		int ggt =  ggt(nenner, zaehler);
+		nenner 	/= ggt;
+		zaehler /= ggt;
 	}
 	
 	public int getZaehler() {
@@ -87,10 +127,21 @@ public class Bruch {
 		if (nenner == 0) {
 			throw new IllegalArgumentException("Nenner muss von Null verschieden sein.");
 		}
-
-		/*
-		 * Hier ergaenzen.
-		 */
+		this.nenner = nenner;
+		kuerze();
+	}
+	
+	@Override
+	public boolean equals(Object arg0) {
+//		System.out.println(zaehler + " / " + nenner);
+		if (!(arg0 instanceof Bruch) ){
+			return false;
+		}else if(nenner != ((Bruch) arg0).getNenner()) {
+			return false;
+		}else if(zaehler != ((Bruch) arg0).getZaehler()) {
+			return false;
+		}
+		return true;
 	}
 
 	
--- Bruch/src/BruchTest.java
+++ Bruch/src/BruchTest.java
@@ -6,37 +6,163 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class BruchTest {
+	
+	Bruch einBruch = new Bruch();
+	Bruch einBruch2 = new Bruch(7);
+	Bruch einBruch3 = new Bruch(7,7);
+	Bruch einBruch4 = new Bruch(3,4);
+	Bruch einBruch5 = new Bruch(4,3);
+	Bruch einBruch6 = new Bruch(2,4);
+	
+	Bruch b1PlusB2 = einBruch.add(einBruch2);
+	Bruch b3PlusB4 = einBruch3.add(einBruch4);
+	Bruch b4PlusB5 = einBruch4.add(einBruch5);
+	
+	Bruch b1SubB2 = einBruch.sub(einBruch2);
+	Bruch b3SubB4 = einBruch3.sub(einBruch4);
+	Bruch b4SubB5 = einBruch4.sub(einBruch5);
+	Bruch b4SubB6 = einBruch4.sub(einBruch6);
+	
+	Bruch b1MulB2 = einBruch .mul(einBruch2);
+	Bruch b3MulB4 = einBruch3.mul(einBruch4);
+	Bruch b4MulB5 = einBruch4.mul(einBruch5);
+	Bruch b4MulB6 = einBruch4.mul(einBruch6);
+	
+	Bruch b1DivB2 = einBruch .div(einBruch2);
+	Bruch b3DivB4 = einBruch3.div(einBruch4);
+	Bruch b4DivB5 = einBruch4.div(einBruch5);
+	Bruch b4DivB6 = einBruch4.div(einBruch6);
 
     /* Ein beispielhafter Test einer Ausnahmebehandlung: */
 	@Test
 	public void testKonstruktor() {
 
-		// Teste das Ausloesen einer Ausnahme bei Null als Nenner
+		// Teste das Ausloesen einer Ausnahme bei Null als Nenner. Da der Konstruktor setNenner aufruhft muss setNenner nichtmehr getestet werden.
 		Throwable ex = assertThrows(IllegalArgumentException.class, () -> {new Bruch(42, 0);});
 		assertThat("Inkorrekte Ausnahme geworfen.",
 		        ex.getMessage(),
 		        is(equalTo("Nenner muss von Null verschieden sein.")));
-
-		/*
-		 * Hier Tests ergaenzen.
-		 */
+		assertThat("Konstruktor arbeitet nicht korrekt.",
+				einBruch,
+				is(equalTo(new Bruch(1,1))));
+		assertThat("Konstruktor arbeitet nicht korrekt.",
+				einBruch2,
+				is(equalTo(new Bruch(7, 1))));
+		assertThat("Konstruktor arbeitet nicht korrekt.",
+				einBruch3,
+				is(equalTo(einBruch)));
 	}
 	
 	/* Ein beispielhafter Test: */
 	@Test
 	public void testGetZaehler() {
 		// Erzeuge einen Bruch zum Testen.
-		Bruch einBruch = new Bruch();
-
 		// Teste den Zaehler des Bruchs.
 		assertThat("getZahler arbeitet nicht korrekt.",
 				einBruch.getZaehler(),
 				is(equalTo(1)));
-
+		assertThat("getZahler arbeitet nicht korrekt.",
+				einBruch2.getZaehler(),
+				is(equalTo(7)));
+		assertThat("kuerze arbeitet nicht korrekt",
+				einBruch3.getZaehler(),
+				is(equalTo(1)));
+		assertThat("getZahler arbeitet nicht korrekt.",
+				einBruch4.getZaehler(),
+				is(equalTo(3)));
+		assertThat("getNenner arbeitet nicht korrekt.",
+				einBruch4.getZaehler(),
+				is(equalTo(3)));
+		
 	}
+	@Test
+	public void testGetNenner() {
+		// Erzeuge einen Bruch zum Testen.
 
-	/*
-	 * Hier Tests ergaenzen.
-	 */
-
+		// Teste den Zaehler des Bruchs.
+		assertThat("getZahler arbeitet nicht korrekt.",
+				einBruch.getNenner(),
+				is(equalTo(1)));
+		
+	}
+	
+	@Test
+	public void testAdd() {
+		// Erzeuge einen Bruch zum Testen.
+		
+		
+		// Teste den Zaehler des Bruchs.
+		assertThat("add arbeitet nicht korrekt.",
+				b1PlusB2,
+				is(equalTo(new Bruch(8, 1))));
+		assertThat("add arbeitet nicht korrekt.",
+				b3PlusB4,
+				is(equalTo(new Bruch(7, 4))));
+		assertThat("add arbeitet nicht korrekt.",
+				b4PlusB5,
+				is(equalTo(new Bruch(25, 12))));
+		
+	}
+	@Test
+	public void testSub() {
+		// Erzeuge einen Bruch zum Testen.
+		
+		
+		// Teste den Zaehler des Bruchs.
+		assertThat("sub arbeitet nicht korrekt.",
+				b1SubB2,
+				is(equalTo(new Bruch(-6, 1))));
+		assertThat("sub arbeitet nicht korrekt.",
+				b3SubB4,
+				is(equalTo(new Bruch(1, 4))));
+		assertThat("sub arbeitet nicht korrekt.",
+				b4SubB5,
+				is(equalTo(new Bruch(-7, 12))));
+		assertThat("sub arbeitet nicht korrekt.",
+				b4SubB6,
+				is(equalTo(new Bruch(1, 4))));
+		
+	}
+	
+	@Test
+	public void testMul() {
+		// Erzeuge einen Bruch zum Testen.
+		
+		
+		// Teste den Zaehler des Bruchs.
+		assertThat("mul arbeitet nicht korrekt.",
+				b1MulB2,
+				is(equalTo(new Bruch(7, 1))));
+		assertThat("mul arbeitet nicht korrekt.",
+				b3MulB4,
+				is(equalTo(new Bruch(3, 4))));
+		assertThat("mul arbeitet nicht korrekt.",
+				b4MulB5,
+				is(equalTo(new Bruch(1, 1))));
+		assertThat("mul arbeitet nicht korrekt.",
+				b4MulB6,
+				is(equalTo(new Bruch(3, 8))));
+		
+	}
+	
+	@Test
+	public void testDiv() {
+		// Erzeuge einen Bruch zum Testen.
+		
+		
+		// Teste den Zaehler des Bruchs.
+		assertThat("mul arbeitet nicht korrekt.",
+				b1DivB2,
+				is(equalTo(new Bruch(1, 7))));
+		assertThat("mul arbeitet nicht korrekt.",
+				b3DivB4,
+				is(equalTo(new Bruch(4, 3))));
+		assertThat("mul arbeitet nicht korrekt.",
+				b4DivB5,
+				is(equalTo(new Bruch(9, 16))));
+		assertThat("mul arbeitet nicht korrekt.",
+				b4DivB6,
+				is(equalTo(new Bruch(3, 2))));
+		
+	}
 }
